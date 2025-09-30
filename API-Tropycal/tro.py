b0a168cb9a39c1c2e178c876ddb17e3f1edebdc2
Emlli <pedroenriquemendoz@live.com.mx>
2025-09-30 01:38:35 -0600

    mejora en la verificacion de tormentas, y solucion de errores

--- API-Tropycal/tro.py
+++ API-Tropycal/tro.py
@@ -3,8 +3,7 @@
 # []
 
 
-from fastapi.responses import FileResponse
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse
 from tropycal import realtime
 from fastapi import FastAPI
 import datetime
@@ -40,6 +39,32 @@ def get_color(vmax):
         elif vmax < 113: return "red"
         elif vmax < 137: return "purple"
         else: return "black"
+
+def verificar_tormenta(storm_name, fecha):
+    dir = os.path.join("data", "json", fecha, "Tormentas")
+    os.makedirs(dir, exist_ok=True)
+    filename = f"data_tormentas.json"
+    filepath = os.path.join(dir, filename)
+
+    if os.path.exists(filepath):
+        with open(filepath, "r") as archivo:
+            data_storm = json.load(archivo)
+            data_storm = data_storm["Info Tormentas"]
+            if storm_name in data_storm:
+                return True
+            else:
+                return False
+    
+    realtime_obj = realtime.Realtime()
+    lista_tormentas = realtime_obj.list_active_storms()
+    tormentas = {"Info Tormentas": lista_tormentas}
+    with open(filepath, "w") as archivo:
+        json.dump(tormentas, archivo, indent=3, default=str)
+    
+    if storm_name in data_storm:
+        return True
+    return False
+
 
 
 # Rutas de tormentas totales ------------------------------------------------------------------
@@ -90,6 +115,12 @@ def get_all_storms():
 @app.get("/data/{storm_name}")
 def get_data(storm_name: str):
     fecha = datetimestring()
+    if not verificar_tormenta(storm_name, fecha):
+        return PlainTextResponse(
+            content=f"No existe tormenta con el nombre {storm_name}",
+            status_code=404)
+
+
     dir = os.path.join("data", "json", fecha, storm_name)
     os.makedirs(dir, exist_ok=True)
     
@@ -116,13 +147,18 @@ def get_data(storm_name: str):
 @app.get("/images/{storm_name}")
 def get_storm_image(storm_name: str):
     fecha = datetimestring()
+    if not verificar_tormenta(storm_name, fecha):
+        return PlainTextResponse(
+            content=f"No existe tormenta con el nombre {storm_name}",
+            status_code=404)
+    
     dir = os.path.join("data", "images", fecha, storm_name)
     os.makedirs(dir, exist_ok=True)
 
     filename = f"{storm_name}.png"
-    filepath = os.path.join("storms", filename)
+    filepath = os.path.join(dir, filename)
 
-    if not os.path.exists(filepath):
+    if os.path.exists(filepath):
         return FileResponse(filepath, media_type="image/png")
 
     realtime_obj = realtime.Realtime()
@@ -132,29 +168,18 @@ def get_storm_image(storm_name: str):
     return FileResponse(filepath, media_type="image/png")
 
 
-# En proceso ------------------------------------------------------------------
-
 @app.get("/dynamic/{storm_name}")
 def prueba(storm_name: str):
     fecha = datetimestring()
+    if not verificar_tormenta(storm_name, fecha):
+        return PlainTextResponse(
+            content=f"No existe tormenta con el nombre {storm_name}",
+            status_code=404)
+
     dir = os.path.join("data", "dynamic", fecha, storm_name)
     os.makedirs(dir, exist_ok=True)
-    print(fecha)
-
     filename = f"{storm_name}.html"
     filepath = os.path.join(dir, filename)
-    json_path = os.path.join("data","json", fecha, "Tormentas", "data_tormentas.json")
-
-    with open(json_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        nombres_tormentas = data.get("Info Tormentas", [])
-
-    if storm_name not in nombres_tormentas:
-        return HTMLResponse(
-            content=f"<h2>No existe tormenta con nombre {storm_name}</h2>",
-            status_code=404
-        )
-
 
     if os.path.exists(filepath):
         with open(filepath, "r", encoding="utf-8") as file:
